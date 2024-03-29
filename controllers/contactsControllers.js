1b5c1987cd4ef50a8d9038775e39494bd87fd20a
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-03-29 18:46:36 +0200

    added nanoid

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,6 +1,9 @@
 import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = async (req, res) => {};
+export const getAllContacts = async (req, res) => {
+    const result = await contacts.listContacts();
+    res.json(result);
+};
 
 export const getOneContact = (req, res) => {};
 
