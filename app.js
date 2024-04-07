68d7f6422fd90253133221011b8c6149c6e47e80
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-04-07 22:24:10 +0300

    added file server

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import mongoose from "mongoose";
 import "dotenv/config";
 import contactsRouter from "./routes/contactsRouter.js";
 
-mongoose.set("strictQuery", true);
-const { DB_HOST, PORT = 3000 } = process.env;
-
-const app = express();
+export const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
@@ -24,18 +20,3 @@ app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
-
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT);
-    console.log("Database connection successful");
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
-
-// app.listen(3000, () => {
-//   console.log("Server is running. Use our API on port: 3000");
-// });
