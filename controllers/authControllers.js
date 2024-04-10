aaf19fd091de30eeae68a80b5effe178ec5dc971
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-04-10 18:30:42 +0300

    added hash password

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,16 +1,19 @@
+import bcrypt from "bcrypt";
 import { User } from "../models/users.js";
 import HttpError from "../helpers/HttpError.js";
 import { ctrlWrapper } from "../helpers/ctrlWrapper.js";
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
     throw HttpError(409, "Email  in use");
   }
 
-  const newUser = await User.create(req.body);
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     email: newUser.email,
