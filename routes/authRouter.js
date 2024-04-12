37fefe1c74e81d1dbfc2934ce0105857257fc35e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2024-04-13 00:57:46 +0300

    added current user

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import { schemas } from "../models/users.js";
 import ctrl from "../controllers/authControllers.js";
+import { authenticate } from "../middlewares/authenticate.js";
 
 const authRouter = express.Router();
 
@@ -12,5 +13,7 @@ authRouter.post(
 );
 
 authRouter.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
+authRouter.get("/current", authenticate, ctrl.getCurrent);
 
 export default authRouter;
